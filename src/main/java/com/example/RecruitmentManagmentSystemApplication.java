0a5d461e3161c5694f3a46c42854de1c25f34d67
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2021-11-17 18:05:06 +0100

    Maryam_OffreEmploi_Cv_CRUD

--- src/main/java/com/example/RecruitmentManagmentSystemApplication.java
+++ src/main/java/com/example/RecruitmentManagmentSystemApplication.java
@@ -1,13 +1,45 @@
 package com.example;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RecruitmentManagmentSystemApplication {
+import com.example.dao.CVRepository;
+import com.example.dao.OffreEmploiRepository;
+import com.example.entities.Cv;
+import com.example.entities.OffreEmploi;
 
+@SpringBootApplication
+public class RecruitmentManagmentSystemApplication implements CommandLineRunner{
+
+	@Autowired
+	private OffreEmploiRepository OffreRep;
+	@Autowired
+	private CVRepository CvRep;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RecruitmentManagmentSystemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		//Ajouter deux offres
+		OffreEmploi Offre1 = new OffreEmploi(1,"Offre1","Description Offre 1", 10,new Date());
+		OffreEmploi Offre2 = new OffreEmploi(2,"Offre2","Description Offre 2", 20,new Date());
+		OffreRep.save(Offre1);
+		OffreRep.save(Offre2);
+		
+		//Ajouter deux cv
+		Cv cv1 = new Cv(1,"cv1.pdf");
+		Cv cv2 = new Cv(2,"cv2.pdf");
+		CvRep.save(cv1);
+		CvRep.save(cv2);
+		
 	}
 
 }
