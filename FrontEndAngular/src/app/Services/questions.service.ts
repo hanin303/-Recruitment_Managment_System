cc70d528f56591e68f344e675ac8d0dd9fc767fe
NourG8 <nourgerfali08@gmail.com>
2021-12-31 13:12:12 +0100

    Nour_Version_Final

--- FrontEndAngular/src/app/Services/questions.service.ts
+++ FrontEndAngular/src/app/Services/questions.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { questionList } from '../Models/questions';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,26 +10,41 @@ import { questionList } from '../Models/questions';
 export class QuestionsService {
 
   private baseUrl = 'http://localhost:3800/Questions';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private authService : AuthService) { }
 
   getQuestionsList(): Observable<questionList[]> {
-    return this.http.get<questionList[]>(`${this.baseUrl}`);
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt}) ;
+    return this.http.get<questionList[]>(`${this.baseUrl}`,{headers:httpHeaders});
    }
   getQuestions(id: number): Observable<questionList> {
-    return this.http.get<questionList>(`${this.baseUrl}/${id}`);
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt}) ;
+    return this.http.get<questionList>(`${this.baseUrl}/${id}`,{headers:httpHeaders});
   }
 
    createQuestions(questions: Object): Observable<Object> {
-     return this.http.post(`${this.baseUrl}`, questions);
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt}) ;
+     return this.http.post(`${this.baseUrl}`, questions,{headers:httpHeaders});
    }
 
    updateQuestions(id: number, value: any): Observable<Object> {
-     return this.http.put(`${this.baseUrl}/${id}`, value);
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt}) ;
+     return this.http.put(`${this.baseUrl}/${id}`, value,{headers:httpHeaders});
    }
 
    deleteQuestions(id: number): Observable<any> {
-     return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt}) ;
+     return this.http.delete(`${this.baseUrl}/${id}`,{headers:httpHeaders});
    }
 
-  
+
 }
